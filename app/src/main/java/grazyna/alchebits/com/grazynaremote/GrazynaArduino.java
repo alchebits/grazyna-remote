e758d2991cc652d2249bb98dff6f9f518aff1ca1
Dariusz Murawski <darek645@gmail.com>
2017-06-18 21:51:26 +0200

    small bt changes

--- app/src/main/java/grazyna/alchebits/com/grazynaremote/GrazynaArduino.java
+++ app/src/main/java/grazyna/alchebits/com/grazynaremote/GrazynaArduino.java
@@ -8,6 +8,14 @@ public class GrazynaArduino {
     public static final String BT_COMM_DELIMETER = "\r\n";
     public static final String BT_CALIBRATION_ON_MSG = "clON";
     public static final String BT_CALIBRATION_OFF_MSG = "clOFF";
+    public static final String BT_CALIBRATION_KP_PP = "kp++";
+    public static final String BT_CALIBRATION_KI_PP = "ki++";
+    public static final String BT_CALIBRATION_KD_PP = "kd++";
+    public static final String BT_CALIBRATION_DIV_PP = "div++";
+    public static final String BT_CALIBRATION_KP_MM = "kp--";
+    public static final String BT_CALIBRATION_KI_MM = "ki--";
+    public static final String BT_CALIBRATION_KD_MM = "kd--";
+    public static final String BT_CALIBRATION_DIV_MM = "div--";
 
     public static final String BT_REC_CALIB_DATA_CMD = "clDt";
 
